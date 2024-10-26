3cc704f243cf4acaa41a203ef0830e62b928aecb
kiganyamburu <mburukiganya@gmail.com>
2024-10-26 17:11:45 +0300

    working on more function

--- Day27/index.js
+++ Day27/index.js
@@ -8,7 +8,14 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
+let countEl = document.getElementById("count-el");
+
+console.log(countEl);
+
+let count = 0;
 
 function increment() {
-  console.log("The button was clicked");
+  count = count + 1;
+
+  console.log(count);
 }
---
+++ Day28/main.js
@@ -0,0 +1,14 @@
+let lapsCompleted = 0;
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function incrementByOne() {
+  lapsCompleted = lapsCompleted + 1;
+}
+
+incrementByOne();
+incrementByOne();
+incrementByOne();
+
+console.log(lapsCompleted);
