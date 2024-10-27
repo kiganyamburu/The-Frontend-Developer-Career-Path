1d03c4ba6c34bc3ca8e5e61f5b5da3f73e13add0
kiganyamburu <mburukiganya@gmail.com>
2024-10-27 13:22:00 +0300

    change from innerText to TextContent

--- Day27-countApp/index.js
+++ Day27-countApp/index.js
@@ -7,14 +7,14 @@ console.log(saveEl);
 
 function increment() {
   count += 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
 
 function save() {
   let countStr = count + " - ";
   // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
   // 3. Render the variable in the saveEl using innerText
-  saveEl.innerText += countStr;
+  saveEl.textContent += countStr;
   // NB: Make sure to not delete the existing content of the paragraph
   console.log(count);
 }
