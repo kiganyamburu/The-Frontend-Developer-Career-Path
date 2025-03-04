3239e68cc8ef34a71868bf9a5ae86d12c9c25442
kiganyamburu <mburukiganya@gmail.com>
2025-03-04 20:56:43 +0300

    js

--- Day25/main.js
+++ Day25/main.js
@@ -1,5 +1,10 @@
-// document.getElementById("count-el").innerText = 5
+document.getElementById("count-el").innerText = 5
 
 let count = 0;
 
 console.log(count);
+
+function count() {
+    let countEl = document.getElementById("count");
+
+}
