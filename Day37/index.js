b12ed0d86481cc5f3e3dbcbaa05be835ebfefd4b
kiganyamburu <mburukiganya@gmail.com>
2025-03-13 15:52:09 +0300

    more practice on conditional statements

--- Day37/index.js
+++ Day37/index.js
@@ -1,6 +1,13 @@
 // Check if the person is elegible for a birthday card from the King! (100)
 
-let age = 100
+let age = 101;
+if (age < 100) {
+    console.log("NOt elegible for a birthday card")
+} else if (age === 100) {
+    console.log("Here is your birthday card from the King!");
+} else {
+    console.log("Not elegible, you have already gotten one")
+}
 
 // if less than 100    -> "Not elegible"
 // else if exactly 100 -> "Here is your birthday card from the King!"
