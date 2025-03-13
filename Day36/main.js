2ded8a185d6cb753c17e64664f55d1a1ae4b93e0
kiganyamburu <mburukiganya@gmail.com>
2025-03-13 15:32:31 +0300

    else

--- Day36/main.js
+++ Day36/main.js
@@ -6,6 +6,6 @@ if (sum < 21) {
     console.log("Do you want to pick a different card?😊");
 } else if (sum === 21) {
     console.log("congratulation you have won the game!🎉");
-} else if  (sum > 21) {
+} else {
     console.log("You have lost the game!😒");
 }
