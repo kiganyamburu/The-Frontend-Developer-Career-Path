b45e0bed547b27f1ec6cc3c993d3c17a7e0f79b5
kiganyamburu <mburukiganya@gmail.com>
2025-03-17 23:17:19 +0300

    concatination

--- Day43/index.js
+++ Day43/index.js
@@ -1,24 +1,25 @@
-    let firstCard = 10
-    let secondCard = 4
-    let sum = firstCard + secondCard
-    let hasBlackJack = false
-    let isAlive = true
-    let message = ""
-    let messageEl = document.getElementById("message-el")
-    let sumEl = document.getElementById("sum-el")
-    
-    function startGame() {
-        if (sum <= 20) {
-            message = "Do you want to draw a new card? "
-        } else if (sum === 21) {
-            message = "Wohoo! You've got Blackjack! "
-            hasBlackJack = true
-        } else {
-            message = "You're out of the game! "
-            isAlive = false
-        }
-        messageEl.textContent = message
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+sumEl.textContent = "sum: " + sum
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? "
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! "
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! "
+        isAlive = false
     }
+    messageEl.textContent = message
+}
 
 
 
