0214dd8523f04ebe01176644cc5133621fae073d
kiganyamburu <mburukiganya@gmail.com>
2025-03-17 23:13:48 +0300

    sum

--- Day43/index.js
+++ Day43/index.js
@@ -5,6 +5,8 @@
     let isAlive = true
     let message = ""
     let messageEl = document.getElementById("message-el")
+    let sumEl = document.getElementById("sum-el")
+    
     function startGame() {
         if (sum <= 20) {
             message = "Do you want to draw a new card? "
