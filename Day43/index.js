51f47b8b1428f6a11fa5fabf5bf7fd4bac3efacb
kiganyamburu <mburukiganya@gmail.com>
2025-03-17 23:47:43 +0300

    removed the uncesarry words

--- Day43/index.js
+++ Day43/index.js
@@ -15,7 +15,7 @@ function startGame() {
     if (sum <= 20) {
         message = "Do you want to draw a new card? "
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! "
+        message = "You've got Blackjack! "
         hasBlackJack = true
     } else {
         message = "You're out of the game! "
