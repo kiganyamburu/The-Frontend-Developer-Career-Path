efb25a343e7dab0d09ab6d51090ecaed0b603c7f
kiganyamburu <mburukiganya@gmail.com>
2025-03-17 23:45:50 +0300

    forgot to add # in the queryselector

--- Day43/index.js
+++ Day43/index.js
@@ -7,7 +7,7 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 // let sumEl = document.getElementById("sum-el")
 let sumEl = document.querySelector("#sum-el")
-let cardEl = document.querySelector("cards-el")
+let cardEl = document.querySelector("#cards-el")
 
 cardEl.textContent = "Card: " + firstCard + ", " + secondCard
 sumEl.textContent = "sum: " + sum
