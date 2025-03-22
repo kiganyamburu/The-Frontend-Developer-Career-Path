d8f457ae929e96cc6ab3878c855e8eae0fc7b4c8
kiganyamburu <mburukiganya@gmail.com>
2025-03-22 16:04:05 +0300

    if statement

--- Day43-Blackjack/index.js
+++ Day43-Blackjack/index.js
@@ -11,7 +11,14 @@ let cardsEl = document.getElementById("cards-el")
 
 // getRandomCard function
 function getRandomCard() {
-    return Math.floor(Math.random() * 13) + 1
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 // startGame() function 
 function startGame() {
