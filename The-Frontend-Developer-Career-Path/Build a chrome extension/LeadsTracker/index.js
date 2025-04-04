3fb79d6a29f29af6beeb17c9fbcc77a3e61e1a21
kiganyamburu <mburukiganya@gmail.com>
2025-04-04 23:14:55 +0300

    .value

--- The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
+++ The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
@@ -3,6 +3,6 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function () {
-    myLeads.push("www.awesomelead.com")
+    myLeads.push(inputEl.value)
     console.log(myLeads)
 })
