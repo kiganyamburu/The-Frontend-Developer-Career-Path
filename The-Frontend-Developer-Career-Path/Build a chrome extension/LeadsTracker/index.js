0cc5599d4c2341aff6b59a167d3c8c90eb766d73
kiganyamburu <mburukiganya@gmail.com>
2025-05-01 15:30:29 +0300

    ancher tag

--- The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
+++ The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
@@ -1,21 +1,25 @@
-let myLeads = [];
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function () {
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    inputEl.value = "";
+    inputEl.value = ""
     renderLeads()
 })
 
 function renderLeads() {
-    let listItems = ""
-for (let i = 0; i < myLeads.length; i++){
-    listItems += "<li>" + myLeads[i] + "</li>" 
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        // Wrap the lead in an anchor tag (<a>) with target="_blank"
+        listItems += `
+            <li>
+                <a target="_blank" href="${myLeads[i]}">
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `;
+    }
+    ulEl.innerHTML = listItems;
 }
-ulEl.innerHTML = listItems
-    
-}
-
-
