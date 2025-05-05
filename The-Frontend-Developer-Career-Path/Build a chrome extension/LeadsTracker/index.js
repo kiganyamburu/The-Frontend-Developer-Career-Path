f61916d893098166d0e03bc88f28ed49a9a2c46d
kiganyamburu <mburukiganya@gmail.com>
2025-05-05 21:22:47 +0300

    checking if its working

--- The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
+++ The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
@@ -14,9 +14,8 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-
-
     renderLeads()
+    console.log( localStorage.getItem("myLeads") )
 })
 
 function renderLeads() {
