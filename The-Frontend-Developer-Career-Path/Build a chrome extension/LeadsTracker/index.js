b5d8ab0756f6b58577ee6b75dbbe0c1c474e4c69
kiganyamburu <mburukiganya@gmail.com>
2025-05-06 06:45:07 +0300

    deletebtn is working now

--- The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
+++ The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
@@ -4,6 +4,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
@@ -11,7 +12,12 @@ if (leadsFromLocalStorage) {
     renderLeads()
 }
 
-
+deleteBtn.addEventListener("dblclick", function () {
+    console.log("double clicked")
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
