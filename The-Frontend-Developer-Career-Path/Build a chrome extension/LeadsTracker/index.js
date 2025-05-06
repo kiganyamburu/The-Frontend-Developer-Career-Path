e6f48544c53d195d30f8ac9805632b5480a0e5dc
kiganyamburu <mburukiganya@gmail.com>
2025-05-06 07:04:40 +0300

    changed myLads

--- The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
+++ The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
@@ -1,32 +1,31 @@
 let myLeads = []
-
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads(myLeads)
+    render(myLeads)
 }
+
 function render(leads) {
-    let listItems = "";
+    let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target="_blank" href="${leads[i]}">
+                <a target='_blank' href='${leads[i]}'>
                     ${leads[i]}
                 </a>
             </li>
-        `;
+        `
     }
-    ulEl.innerHTML = listItems;
+    ulEl.innerHTML = listItems
 }
 
-deleteBtn.addEventListener("dblclick", function () {
-    console.log("double clicked")
+deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
@@ -35,10 +34,6 @@ deleteBtn.addEventListener("dblclick", function () {
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads(myLeads)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
-
-
-
-
