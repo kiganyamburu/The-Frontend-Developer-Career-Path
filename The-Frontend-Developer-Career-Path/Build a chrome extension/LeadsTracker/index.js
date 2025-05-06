4b65e3d0a17042ca941acf1538e99ad79e760d91
kiganyamburu <mburukiganya@gmail.com>
2025-05-06 06:56:58 +0300

    changed myLeads to leads

--- The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
+++ The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
@@ -9,37 +9,36 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    renderLeads(myLeads)
 }
-
-deleteBtn.addEventListener("dblclick", function () {
-    console.log("double clicked")
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-})
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-    console.log( localStorage.getItem("myLeads") )
-})
-
-function renderLeads() {
+function render(leads) {
     let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
-        // Wrap the lead in an anchor tag (<a>) with target="_blank"
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target="_blank" href="${myLeads[i]}">
-                    ${myLeads[i]}
+                <a target="_blank" href="${leads[i]}">
+                    ${leads[i]}
                 </a>
             </li>
         `;
     }
     ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+    console.log("double clicked")
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads)
+})
+
+
 
 
