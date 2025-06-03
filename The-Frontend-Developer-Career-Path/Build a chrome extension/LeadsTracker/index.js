3280b63ebe72bed454e248e74bb16859c120087a
kiganyamburu <mburukiganya@gmail.com>
2025-06-03 09:36:55 +0300

    fix(index.js):Use the Chrome API to get the tab

--- The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
+++ The-Frontend-Developer-Career-Path/Build a chrome extension/LeadsTracker/index.js
@@ -1,17 +1,33 @@
-let myLeads = [];
-let oldLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
-const deleteBtn = document.getElementById("delete-btn");
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  render(myLeads);
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
 
-function renderLeads(leads) {
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    let activeTab = tabs[0];
+    let activeTabId = activeTab.id;
+  });
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+    
+})
+
+function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
@@ -28,12 +44,12 @@ function renderLeads(leads) {
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
-    renderLeads(myLeads)
+    render(myLeads)
 })
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads(myLeads)
+    render(myLeads)
 })
