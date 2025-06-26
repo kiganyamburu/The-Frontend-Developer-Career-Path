f92a60c8fd08468549dcff4e57f07941ecebfae8
kiganyamburu <mburukiganya@gmail.com>
2025-06-26 20:14:36 +0300

    feat(build app)

---
+++ The-Frontend-Developer-Career-Path/Build a mobile App/index.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+
+
+
+const firebaseConfig = {
+   database:"https://leads-tracker-app-f1bb9-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+  
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    render(myLeads)
+})
